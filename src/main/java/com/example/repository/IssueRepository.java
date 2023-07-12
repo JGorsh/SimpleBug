1d0a56a17a550d4d2d4dd16d7e60f60d98d2b8b9
asgor <alexmilesjg@gmail.com>
2023-07-12 10:18:49 +0500

    addd docker compose

--- src/main/java/com/example/repository/IssueRepository.java
+++ src/main/java/com/example/repository/IssueRepository.java
@@ -3,9 +3,12 @@ package com.example.repository;
 import com.example.model.domain.Issue;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
 
 
 public interface IssueRepository extends JpaRepository<Issue, Long>, JpaSpecificationExecutor<Issue> {
@@ -13,4 +16,8 @@ public interface IssueRepository extends JpaRepository<Issue, Long>, JpaSpecific
     @EntityGraph(attributePaths = {"project", "person"})
     @Override
     Page<Issue> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"project", "person"})
+    @Override
+    List<Issue> findAll(Specification<Issue> specification);
 }
